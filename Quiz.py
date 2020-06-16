cca7f867ffe5bdedc3b3a57be8ab81b95307596b
Ludkip <62809236+Ludkip@users.noreply.github.com>
2020-06-16 21:05:50 +0200

    updated all

--- Quiz.py
+++ Quiz.py
@@ -1,16 +1,31 @@
 import random
 import json
+qnr = 0
+qlimit = False
 
 with open('questions.json') as f:  
     data =json.load(f)
 
 
-live = random.choice (data['questions'])
-print (live['question'])
-print (live['alt'])
-answer = input()
-if answer == live['correcta']:
-    print('correct answer')
 
-else:
-    print('wrong answer')
+while qlimit == False:
+    live = random.choice (data['questions'])
+    if (live['used']) == False:
+        (live['used']) = True
+        qnr = qnr + 1
+        print (live['question'])
+        print (live['alt'])
+        answer = input()
+        if answer == live['correcta']:
+            print('correct answer')
+
+        else:
+            print('wrong answer')
+
+    else:
+        qnr = qnr
+    
+    if qnr > 3:
+        qlimit = True
+
+print ("Congratulations")
