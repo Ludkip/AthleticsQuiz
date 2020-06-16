3cf45e9869e75dcabe763674a81cfe1ffc318cd8
Ludkip <62809236+Ludkip@users.noreply.github.com>
2020-06-16 19:50:29 +0200

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,5 +1,15 @@
 import random
+import json
 
+with open('questions.json') as f:  
+    data =json.load(f)
+
+#for q in data['questions']:
+    #print (q['question'], q['alt'])
+
+live = random.choice in data['questions']
+print (live)
+'''  
 questions = ["q1", "q2", "q3"]
 
 qu1 = {"question" : "What is the mens pole vault world record?", "correcta" : "b", "alt" : {"a" : "a. 6 m", "b" : "b. 6,18 m","c" : "c. 5,54 m", "d" : "d. 20,71 m"}}
@@ -14,6 +24,7 @@ q3a =  ["a. 200 m", "b. 100 m", "c. 300 m", "d. 400 m"]
 #qvalue = random.choice(questions)
 qvalue="q1"
 
+
 if qvalue == "q1":
     print (qu1["question"])
     
@@ -25,7 +36,7 @@ if qvalue == "q1":
 
     else:
         print ("Wrong answer")
-"""
+
 elif qvalue == "q2":
     print (q2)
     print (q2a)
@@ -46,4 +57,4 @@ if qvalue == ("q3"):
     else:
         print ("Wrong answer")
     
-"""
+'''
